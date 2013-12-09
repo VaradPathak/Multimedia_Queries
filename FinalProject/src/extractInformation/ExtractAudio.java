7da53d6319634e0e6b6184e7d2c3e8d76a9ab50a
VaradPathak <pathak.varad@gmail.com>
2013-12-08 20:45:52 -0800

    deleted unused FFT files

--- FinalProject/src/extractInformation/ExtractAudio.java
+++ FinalProject/src/extractInformation/ExtractAudio.java
@@ -3,15 +3,8 @@ package extractInformation;
 import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
-
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.UnsupportedAudioFileException;
-
-import playWaveFile.PlayWaveException;
 
 import com.musicg.wave.Wave;
 import com.musicg.wave.extension.Spectrogram;
@@ -54,47 +47,47 @@ public class ExtractAudio {
 		}
 	}
 
-	public void extractAudio(List<Double[]> audioFeatureList)
-			throws PlayWaveException {
-
-		AudioInputStream audioInputStream = null;
-		try {
-			audioInputStream = AudioSystem.getAudioInputStream(this.waveStream);
-		} catch (UnsupportedAudioFileException e1) {
-			throw new PlayWaveException(e1);
-		} catch (IOException e1) {
-			throw new PlayWaveException(e1);
-		}
-
-		int readBytes = 0;
-		byte[] audioBuffer = new byte[this.EXTERNAL_BUFFER_SIZE];
-
-		try {
-			int k = 0;
-			while (readBytes != -1) {
-				readBytes = audioInputStream.read(audioBuffer, 0,
-						audioBuffer.length);
-				if (readBytes >= 0 && k % 8 == 0) {
-					Complex[] complexData = new Complex[audioBuffer.length];
-					for (int i = 0; i < complexData.length; i++) {
-						complexData[i] = new Complex(audioBuffer[i], 0);
-					}
-					Complex[] fftResult = FFT.fft(complexData);
-					Double result[] = new Double[fftResult.length / 16];
-
-					for (int i = 0, j = 0; i < fftResult.length; i += 16, j++) {
-						result[j] = Math.sqrt((fftResult[i].re() * fftResult[i]
-								.re())
-								+ (fftResult[i].im() * fftResult[i].im()));
-						result[j] /= 10000;
-					}
-					audioFeatureList.add(result);
-				}
-				k++;
-			}
-		} catch (IOException e1) {
-			throw new PlayWaveException(e1);
-		}
-	}
+//	public void extractAudio(List<Double[]> audioFeatureList)
+//			throws PlayWaveException {
+//
+//		AudioInputStream audioInputStream = null;
+//		try {
+//			audioInputStream = AudioSystem.getAudioInputStream(this.waveStream);
+//		} catch (UnsupportedAudioFileException e1) {
+//			throw new PlayWaveException(e1);
+//		} catch (IOException e1) {
+//			throw new PlayWaveException(e1);
+//		}
+//
+//		int readBytes = 0;
+//		byte[] audioBuffer = new byte[this.EXTERNAL_BUFFER_SIZE];
+//
+//		try {
+//			int k = 0;
+//			while (readBytes != -1) {
+//				readBytes = audioInputStream.read(audioBuffer, 0,
+//						audioBuffer.length);
+//				if (readBytes >= 0 && k % 8 == 0) {
+//					Complex[] complexData = new Complex[audioBuffer.length];
+//					for (int i = 0; i < complexData.length; i++) {
+//						complexData[i] = new Complex(audioBuffer[i], 0);
+//					}
+//					Complex[] fftResult = FFT.fft(complexData);
+//					Double result[] = new Double[fftResult.length / 16];
+//
+//					for (int i = 0, j = 0; i < fftResult.length; i += 16, j++) {
+//						result[j] = Math.sqrt((fftResult[i].re() * fftResult[i]
+//								.re())
+//								+ (fftResult[i].im() * fftResult[i].im()));
+//						result[j] /= 10000;
+//					}
+//					audioFeatureList.add(result);
+//				}
+//				k++;
+//			}
+//		} catch (IOException e1) {
+//			throw new PlayWaveException(e1);
+//		}
+//	}
 
 }
